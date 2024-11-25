b4d78901e3c65e678aeb692211b64bd0ccaa7d26
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-11-26 00:35:42 +0900

    welcome screen ui

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -14,8 +14,8 @@ class WelcomeScreen extends StatelessWidget {
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              Colors.orange[100]!,
-              Colors.tealAccent,
+              const Color(0xffeeedd5),
+              const Color(0xff6adedb),
             ],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
@@ -27,12 +27,20 @@ class WelcomeScreen extends StatelessWidget {
           child: Column(
             children: [
               const Spacer(),
-              Center(
-                child: Image.asset(
-                  "assets/images/talk_diary_image_logo.png",
-                  width: 150,
-                  height: 150,
-                ),
+              Column(
+                children: [
+                  Image.asset(
+                    "assets/images/talk_diary_writing_logo.png",
+                    fit: BoxFit.fitHeight,
+                    // width: 300,
+                    // height: 300,
+                  ),
+                  Image.asset(
+                    "assets/images/talk_diary_image_logo.png",
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
               ),
               const Spacer(),
               Align(
@@ -46,30 +54,102 @@ class WelcomeScreen extends StatelessWidget {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Expanded(
-                        flex: 1,
+                      Container(
+                        width: 160,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: const Color(0xffF86B61),
+                            width: 3,
+                          ),
+                          boxShadow: [],
+                        ),
                         child: TextButton(
                           onPressed: () {},
-                          child: const Text("로그인",
+                          child: Text("로그인",
                               style: TextStyle(
+                                color: const Color(0xffF86B61),
                                 fontWeight: FontWeight.bold,
                                 fontSize: 20,
                               )),
                         ),
                       ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        flex: 1,
+                      const SizedBox(width: 20),
+                      Container(
+                        width: 160,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: const Color(0xfffd420b),
+                          gradient: LinearGradient(
+                            colors: [
+                              const Color(0xffEDE5C8),
+                              const Color(0xffF86B61),
+                            ],
+                            begin: Alignment.topCenter,
+                            stops: [0.0, 0.5],
+                            end: Alignment.bottomCenter,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              blurRadius: 5,
+                              offset: Offset(2, 2),
+                            ),
+                          ],
+                        ),
                         child: TextButton(
                           onPressed: () {},
                           child: const Text("회원가입",
                               style: TextStyle(
+                                color: Colors.white,
                                 fontWeight: FontWeight.bold,
                                 fontSize: 20,
                               )),
                         ),
                       ),
                     ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Center(
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'By pressing “Sign Up” you agree to our ',
+                          style: TextStyle(
+                            color: Color(0xff00444C),
+                            fontSize: 14,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Privacy Policy\n',
+                          style: TextStyle(
+                            color: Color(0xffff440d),
+                            fontSize: 14,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'and ',
+                          style: TextStyle(
+                            color: Color(0xff00444C),
+                            fontSize: 14,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Terms and Service',
+                          style: TextStyle(
+                            color: Color(0xffff440d),
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center, // 텍스트 가운데 정렬
                   ),
                 ),
               ),
