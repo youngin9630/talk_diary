64abb007c353afa1acff8c752523567015c18cef
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-11-27 14:38:31 +0900

    user.dart -> email 추가

--- lib/data/user.dart
+++ lib/data/user.dart
@@ -1,9 +1,11 @@
 abstract class User {
+  final String _email; // private 필드
   final String _username; // private 필드
   final String _password; // private 필드
 
-  User(this._username, this._password);
+  User(this._email, this._username, this._password);
 
+  String get email => _email; // 읽기 전용 getter 제공
   String get username => _username; // 읽기 전용 getter 제공
   String get password => _password; // 읽기 전용 getter 제공
 }
