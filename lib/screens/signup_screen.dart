7d0177980adbb5b291d5923a7beb9dc005e59460
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-11-27 13:35:46 +0900

    signup_screen.dart -> 이메일, 비밀번호, 비밀번호확인 텍스트폼필드 생성 / 회원가입 버튼 클릭시 main_screen.dart로 이동

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:talk_diary/main_screen.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -9,34 +10,6 @@ class SignupScreen extends StatefulWidget {
 }
 
 class SignupScreenState extends State<SignupScreen> {
-  String apiResponse = "Loading...";
-
-  @override
-  void initState() {
-    super.initState();
-    fetchApiData();
-  }
-
-  Future<void> fetchApiData() async {
-    try {
-      final response =
-          await http.get(Uri.parse('http://13.125.128.111:8080/api/test'));
-      if (response.statusCode == 200) {
-        setState(() {
-          apiResponse = response.body; // JSON 데이터에서 필요한 부분 추출
-        });
-      } else {
-        setState(() {
-          apiResponse = "Failed to load data";
-        });
-      }
-    } catch (e) {
-      setState(() {
-        apiResponse = "Error: $e";
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,9 +23,10 @@ class SignupScreenState extends State<SignupScreen> {
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
+                      SizedBox(height: 24),
                       TextFormField(
                         decoration: InputDecoration(
-                          hintText: "이름",
+                          hintText: "이메일",
                           hintStyle: TextStyle(
                             color: Color(0xff999999),
                           ),
@@ -65,11 +39,12 @@ class SignupScreenState extends State<SignupScreen> {
                             borderSide: BorderSide(color: Colors.red),
                           ),
                         ),
+                        keyboardType: TextInputType.emailAddress,
                       ),
                       SizedBox(height: 16),
                       TextFormField(
                         decoration: InputDecoration(
-                          hintText: "이름",
+                          hintText: "비밀번호",
                           hintStyle: TextStyle(
                             color: Color(0xff999999),
                           ),
@@ -82,11 +57,38 @@ class SignupScreenState extends State<SignupScreen> {
                             borderSide: BorderSide(color: Colors.red),
                           ),
                         ),
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "비밀번호확인",
+                          hintStyle: TextStyle(
+                            color: Color(0xff999999),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(color: Color(0xff999999)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(color: Colors.red),
+                          ),
+                        ),
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
                       ),
                       Row(
                         children: [
                           TextButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MainScreen()),
+                              );
+                            },
                             child: Text('가입완료'),
                           ),
                           TextButton(
@@ -116,5 +118,33 @@ class SignupScreenState extends State<SignupScreen> {
         end: Alignment.bottomCenter,
       ),
     );
+  }
+
+  String apiResponse = "Loading...";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchApiData();
+  }
+
+  Future<void> fetchApiData() async {
+    try {
+      final response =
+          await http.get(Uri.parse('http://13.125.128.111:8080/api/test'));
+      if (response.statusCode == 200) {
+        setState(() {
+          apiResponse = response.body; // JSON 데이터에서 필요한 부분 추출
+        });
+      } else {
+        setState(() {
+          apiResponse = "Failed to load data";
+        });
+      }
+    } catch (e) {
+      setState(() {
+        apiResponse = "Error: $e";
+      });
+    }
   }
 }
